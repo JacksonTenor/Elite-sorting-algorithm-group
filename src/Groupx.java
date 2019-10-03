f2dee4b23c6be1caf6e1f259cc438e2ee5a229f6
Jackson Tenor <tenor031@umn.edu>
2019-10-03 14:05:51 -0500

    Implement all parts of sorting algorithm and testing

--- src/Groupx.java
+++ src/Groupx.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -5,16 +9,38 @@ import java.util.Collections;
 
 public class Groupx {
     public static void main(String[] args){
-        String[] strings = new String[]{"3/4", "34", "34/1", "3 3/4"};
+        File input = new File("data/data1.txt");
+        ArrayList<String> strings = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(input));
+            while(br.ready()){
+                strings.add(br.readLine());
+            }
+            br.close();
+        }catch(Exception e){
+            System.err.println(e);
+        }
+
         ArrayList<SortingType> sorting = new ArrayList<>();
         for(String s: strings){
             sorting.add(new SortingType(s));
         }
-
-
         Collections.sort(sorting);
-        for(SortingType s: sorting){
-            System.out.println(s.toString());
+
+        File output = new File("data/out1.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(output));
+            int i = 0;
+            while(br.ready()){
+                String line = br.readLine();
+                if(!line.equals(sorting.get(i).toString())){
+                    System.out.println("error");
+                }
+                i++;
+            }
+            br.close();
+        }catch(Exception e){
+            System.err.println(e);
         }
     }
 
--- src/SortingType.java
+++ src/SortingType.java
@@ -2,36 +2,60 @@ public class SortingType implements Comparable<SortingType>{
     private double value;
     private String original;
 
+    //In case of ties, type is used first
+    //0 = Decimal (goes first in ties)
+    //1 = Mixed Fraction (second in ties)
+    //2 = Pure Fraction (third in ties)
+    private int type;
+    //For two Mixed Fractions, the smaller whole number goes first
+    private double whole = 0;
+    //For two Mixed Fractions with the same whole part
+    //or two pure fractions, the smaller numerator goes first.
+    private double numerator = 0;
+
+
     SortingType(String o){
         this.original = o;
         if(o.contains("/")){
             String[] oa = o.split(" ");
             if(oa.length == 2){
-                this.value += Double.parseDouble(oa[0]);
+                this.type=1; //Mixed Fraction
+                double whole = Double.parseDouble(oa[0]);
+                this.value += whole;
+                this.whole = whole;
                 String[] fraction = oa[1].split("/");
-                this.value += Double.parseDouble(fraction[0]) / Double.parseDouble(fraction[1]);
+                this.numerator = Double.parseDouble(fraction[0]);
+                this.value += this.numerator / Double.parseDouble(fraction[1]);
             }else{
+                this.type=2; //Pure Fraction
                 String[] fraction = oa[0].split("/");
-                this.value += Double.parseDouble(fraction[0]) / Double.parseDouble(fraction[1]);
+                this.numerator = Double.parseDouble(fraction[0]);
+                this.value += this.numerator / Double.parseDouble(fraction[1]);
             }
         }else{
+            this.type = 0; //Decimal Expression
             this.value = Double.parseDouble(o);
         }
     }
 
     @Override
     public String toString(){
-        return Double.toString(this.value);
+        //return Double.toString(this.value);
+        return this.original;
     }
 
     @Override
     public int compareTo(SortingType other) {
         int diff = (int) Math.signum(this.value - other.value);
-        if (diff == 0){
-            return diff;
-            //This will not work long term.
-        }else{
-            return diff;
+        if (diff == 0) {
+            diff = this.type - other.type; //Compare by types
+            if(diff == 0 && this.type != 0){ //No further tiebreakers for decimals
+                diff = (int) Math.signum(this.whole - other.whole);
+                if(diff == 0){
+                    diff = (int) Math.signum(this.numerator - other.numerator);
+                }
+            }
         }
+        return diff;
     }
 }
